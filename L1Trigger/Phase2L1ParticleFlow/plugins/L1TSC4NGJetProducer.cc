f51c4663b66bf387e4c62ca505a73757a596a019
Duchstf <minhduc8199@gmail.com>
2025-07-03 16:59:48 -0500

    stable sort for tagged jets

--- L1Trigger/Phase2L1ParticleFlow/plugins/L1TSC4NGJetProducer.cc
+++ L1Trigger/Phase2L1ParticleFlow/plugins/L1TSC4NGJetProducer.cc
@@ -132,7 +132,7 @@ void L1TSC4NGJetProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSe
     edmTaggedJet.addTagScores(JetScore_float, classes_, PtCorrection_);
     taggedJets.push_back(edmTaggedJet);
   }
-  std::sort(taggedJets.begin(), taggedJets.end(), [](l1t::PFJet a, l1t::PFJet b) { return (a.pt() > b.pt()); });
+  std::stable_sort(taggedJets.begin(), taggedJets.end(), [](l1t::PFJet a, l1t::PFJet b) { return (a.pt() > b.pt()); });
 
   auto taggedJetsCollection = std::make_unique<l1t::PFJetCollection>();
   taggedJetsCollection->swap(taggedJets);
